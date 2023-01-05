6517f948cc07debb9efb899776ad906affeebbe7
Yulia Isaeva <pulinu@icloud.com>
2023-01-05 18:16:14 +0500

    fix:change variable name

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 module.exports = function reverse (n) {
-  let newNumber = number.toString().split('').reverse().join('');
+  let newNumber = n.toString().split('').reverse().join('');
   return Number(newNumber);
 }
