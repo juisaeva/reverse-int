0a55dfca17586432a62acdd706d126a5dee84bbe
juisaeva <105415861+juisaeva@users.noreply.github.com>
2025-07-29 19:29:59 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const newNumber = Math.abs(n).toString().split('').reverse().join('');
+  return Number(newNumber);
 };
